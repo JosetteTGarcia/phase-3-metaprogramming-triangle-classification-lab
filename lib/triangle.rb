26819e4a472905f91839f8a15bba910f1059dd29
Josette Garcia <josettetgarcia@gmail.com>
2022-05-19 20:36:19 -0500

    commit tree cc126c932c30a2f18c7a7242bae06a802a4331a0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+attr_reader :side_one, :side_three, :side_two
+
+def initialize(side_one, side_two, side_three)
+  @side_one = side_one
+  @side_two = side_two
+  @side_three = side_three
+end
+
+
+def kind 
+  check_triangle
+  if side_one == side_two && side_two == side_three
+    :equilateral
+  elsif side_one == side_two || side_two == side_three || side_one == side_three
+    :isosceles
+  else side_one != side_two || side_one != side_three || side_three != side_two
+    :scalene
+  end
+
+end
+
+def check_triangle
+
+  if side_one + side_two + side_three == 0 || side_one + side_two > side_three || side_two + side_three > side_one || side_three + side_one > side_two
+    raise TriangleError
+  end
+
+end
+
+
+
+class TriangleError < StandardError
+  # triangle error code
+end
 end
